0927bd5d6cb57aeb0ab50211f8317dafa59158fc
koleks92 <koleks92@gmail.com>
2023-02-08 21:37:07 +0100

    - Eraser button and function added

--- main.py
+++ main.py
@@ -10,23 +10,25 @@ class Drawer():
         root.geometry('1200x800')
         root.title('SuperDrawer')
 
-
         # Drawing area
         self.canvas = Canvas(root, bg='white', width=1100, height=800)
         self.canvas.pack(side=RIGHT)
         self.canvas.bind("<Button-1>", self.save_position)
         self.canvas.bind("<B1-Motion>", self.add_line)
 
-        self.brush_size = 10
-
         # Format Menu Left Side
         self.tool_frame = Frame(root, width=100, height=800, bg='grey')
         self.tool_frame.pack(side=LEFT)
 
-        # size frame and brush size changer
+        # Erase button
+        self.erase_button = Button(self.tool_frame, text = "Eraser", relief="raised", command=lambda: self.erase_function())
+        self.erase_button.pack()
+
+        # Size frame and brush size changer
         self.size_frame = LabelFrame(self.tool_frame, text="Brush size")
         self.size_frame.pack()
 
+        self.brush_size = 10
         self.brush_size = tk.Entry(self.size_frame)
         self.brush_size.insert(END, "10")
         self.brush_size.pack()
@@ -39,9 +41,7 @@ class Drawer():
         self.color_button = Button(self.color_frame, bg = self.paint_color,width=10,height=5, command=lambda: self.color_changer())
         self.color_button.pack()
 
-       
-
-
+    
     #Color changer
     def color_changer(self):
         paint_color = askcolor()
@@ -56,7 +56,15 @@ class Drawer():
     def add_line(self, event):
         self.canvas.create_line(self.lastx, self.lasty, event.x, event.y,width=self.brush_size.get(), fill=self.paint_color)
         self.save_position(event)
-    
+
+    def erase_function(self):
+        if self.erase_button.config('relief')[-1] == 'sunken': # Check if button pressed
+            self.erase_button.config(relief="raised") 
+            copy_color = self.color_button.cget('bg') # Get color before eraser from color_button
+            self.paint_color = copy_color
+        else:
+            self.erase_button.config(relief="sunken")
+            self.paint_color = "white"
 
 
 
@@ -67,9 +75,6 @@ def main():
     root = Tk()
     # Initiate drawer class
     drawer = Drawer(root)
-
-
-
     # Loop for window
     root.mainloop()
 
